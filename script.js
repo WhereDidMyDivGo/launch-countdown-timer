3fd671143fc070208d9c36ccf4b0f82c6c17acee
ahmad <72674221+WhereDidMyDivGo@users.noreply.github.com>
2025-03-18 22:51:59 +0400

    invalid input now takes new input instead of refreshing

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-let days = document.querySelector(".days .block p"),
-  hours = document.querySelector(".hours .block p"),
-  minutes = document.querySelector(".minutes .block p"),
-  seconds = document.querySelector(".seconds .block p"),
-  input = prompt("Enter days (max 99)");
+let input = prompt("Enter days (99 max)");
 
-if (input && !isNaN(input) && input > 0 && input <= 99) {
-  let countdown = input * 86400;
-  function update() {
-    let d = Math.floor(countdown / 86400),
-      h = Math.floor((countdown % 86400) / 3600),
-      m = Math.floor((countdown % 3600) / 60),
-      s = countdown % 60;
+function validate() {
+  if (!input || isNaN(input) || input <= 0 || input > 99) {
+    input = prompt("Invalid input. Enter a number between 1 and 99");
+    validate();
+  } else {
+    let countdown = parseInt(input) * 24 * 60 * 60;
+    const days = document.querySelector(".days .block p");
+    const hours = document.querySelector(".hours .block p");
+    const minutes = document.querySelector(".minutes .block p");
+    const seconds = document.querySelector(".seconds .block p");
 
-    days.innerText = String(d).padStart(2, "0");
-    hours.innerText = String(h).padStart(2, "0");
-    minutes.innerText = String(m).padStart(2, "0");
-    seconds.innerText = String(s).padStart(2, "0");
+    function update() {
+      let d = Math.floor(countdown / 86400),
+        h = Math.floor((countdown % 86400) / 3600),
+        m = Math.floor((countdown % 3600) / 60),
+        s = countdown % 60;
 
-    if ((countdown -= 1) <= 0) clearInterval(timer);
+      days.innerText = String(d).padStart(2, "0");
+      hours.innerText = String(h).padStart(2, "0");
+      minutes.innerText = String(m).padStart(2, "0");
+      seconds.innerText = String(s).padStart(2, "0");
+
+      if ((countdown -= 1) <= 0) clearInterval(timer);
+    }
+    update();
+    let timer = setInterval(update, 1000);
   }
-  update();
-  let timer = setInterval(update, 1000);
-} else {
-  alert("invalid input");
-  location.reload();
 }
+validate();
